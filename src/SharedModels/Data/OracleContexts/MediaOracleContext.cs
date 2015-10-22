fa0196fa611e96a6f7ec19c4a95a624c799c9c68
Wessel Kuipers <wesselkuipers@gmail.com>
2015-10-22 23:19:59 +0200

    Implemented MediaOracleContext

---
+++ src/SharedModels/Data/OracleContexts/MediaOracleContext.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.DataAccess.Client;
+using SharedModels.Data.ContextInterfaces;
+using SharedModels.Enums;
+using SharedModels.Models;
+
+namespace SharedModels.Data.OracleContexts
+{
+    public class MediaOracleContext : EntityOracleContext<Media>, IMediaContext
+    {
+        public List<Media> GetAll()
+        {
+            var query = "SELECT * FROM media ORDER BY mediaid";
+            var res = Database.ExecuteReader(query);
+
+            return res.Select(GetEntityFromRecord).ToList();
+        }
+
+        public Media GetById(object id)
+        {
+            var query = "SELECT * FROM media WHERE mediaid = :mediaid ORDER BY mediaid";
+            var parameters = new List<OracleParameter>
+            {
+                new OracleParameter("mediaid", (int) id)
+            };
+
+            return GetEntityFromRecord(Database.ExecuteReader(query, parameters).First());
+        }
+
+        public Media Insert(Media entity)
+        {
+            var query =
+                "INSERT INTO media (mediaid, userid, eventid, mediatypeid, name, fileextension, filepath) VALUES (seq_media.nextval, :userid, :eventid, :mediatypeid, :name, :fileextension, :filepath) RETURNING mediaid INTO :lastID";
+            var parameters = new List<OracleParameter>
+            {
+                new OracleParameter("userid", entity.UserID),
+                new OracleParameter("eventid", entity.EventID),
+                new OracleParameter("mediatypeid", Convert.ToInt32(entity.Type)),
+                new OracleParameter("name", entity.Name),
+                new OracleParameter("fileextension", entity.FileExtension),
+                new OracleParameter("filepath", entity.Path),
+                new OracleParameter("lastID", OracleDbType.Decimal) {Direction = ParameterDirection.ReturnValue}
+            };
+
+            string newID;
+            if (!Database.ExecuteNonQuery(query, out newID, parameters)) return null;
+            return GetById(Convert.ToInt32(newID));
+        }
+
+        public bool Update(Media entity)
+        {
+            var query = "UPDATE media SET name = :name, fileextension = :fileextension, filepath = :filepath WHERE mediaid = :mediaid";
+            var parameters = new List<OracleParameter>
+            {
+                new OracleParameter("name", entity.Name),
+                new OracleParameter("fileextension", entity.FileExtension),
+                new OracleParameter("filepath", entity.Path),
+                new OracleParameter("mediaid", entity.ID)
+            };
+
+            return Database.ExecuteNonQuery(query, parameters);
+        }
+
+        public bool Delete(Media entity)
+        {
+            var query = "DELETE FROM media WHERE mediaid = :mediaid";
+            var parameters = new List<OracleParameter> { new OracleParameter("mediaid", entity.ID) };
+
+            return Database.ExecuteNonQuery(query, parameters);
+        }
+
+        public List<Media> GetAllByGuest(Guest guest)
+        {
+            var query = "SELECT * FROM media WHERE eventid = :eventid AND userid = :userid ORDER BY mediaid";
+            var parameters = new List<OracleParameter>
+            {
+                new OracleParameter("eventid", guest.EventID),
+                new OracleParameter("eventid", guest.ID)
+            };
+
+            var res = Database.ExecuteReader(query, parameters);
+
+            return res.Select(GetEntityFromRecord).ToList();
+        }
+
+        protected override Media GetEntityFromRecord(List<string> record)
+        {
+            return new Media(Convert.ToInt32(record[0]), Convert.ToInt32(record[1]), Convert.ToInt32(record[2]),
+                (MediaType) Convert.ToInt32(record[3]), record[4], record[5], record[6]);
+        }
+    }
+}
--- src/SharedModels/Models/Media.cs
+++ src/SharedModels/Models/Media.cs
@@ -5,14 +5,27 @@ namespace SharedModels.Models
     public class Media
     {
         public int ID { get; }
-        public string Path { get; set; }
+        public int UserID { get; }
+        public int EventID { get; }
         public MediaType Type { get; set; }
+        public string Name { get; set; }
+        public string FileExtension { get; set; }
+        public string Path { get; set; }
 
-        public Media(int id, string path, MediaType type)
+        public Media(int id, int userId, int eventId, MediaType type, string name, string fileExtension, string path)
         {
             ID = id;
-            Path = path;
+            UserID = userId;
+            EventID = eventId;
             Type = type;
+            Name = name;
+            FileExtension = fileExtension;
+            Path = path;
+        }
+
+        public override string ToString()
+        {
+            return $"Type: {Type}, Name: {Name}.{FileExtension}";
         }
     }
 }
