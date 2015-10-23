a49bf594e009f35b7579c652515ac1b72a5ddedd
Wessel Kuipers <wesselkuipers@gmail.com>
2015-10-23 10:57:18 +0200

    Added initial version of logiccollection class

---
+++ src/SharedModels/Logic/LogicCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedModels.Data.OracleContexts;
+
+namespace SharedModels.Logic
+{
+    /// <summary>
+    /// Helper class used for accessing the user logic globally
+    /// Currently defaults to using Oracle contexts
+    /// </summary>
+    public static class LogicCollection
+    {
+        private static UserLogic _userLogic;
+        public static UserLogic UserLogic => _userLogic ?? (_userLogic = new UserLogic(new UserOracleContext()));
+    }
+}
