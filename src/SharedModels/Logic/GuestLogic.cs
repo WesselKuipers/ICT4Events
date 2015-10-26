3c70ab4bf9e9818960231b4101be55c6d049325e
Wessel Kuipers <wesselkuipers@gmail.com>
2015-10-26 02:05:49 +0100

    Added GuestLogic class

---
+++ src/SharedModels/Logic/GuestLogic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedModels.Data.ContextInterfaces;
+using SharedModels.Models;
+
+namespace SharedModels.Logic
+{
+    public class GuestLogic
+    {
+        private IGuestContext _context;
+
+        public GuestLogic(IGuestContext context)
+        {
+            _context = context;
+        }
+
+        public int GetGuestCountByEvent(Event ev)
+        {
+            return _context.GetGuestCountByEvent(ev);
+        }
+
+        public Guest GetGuestByEvent(Event ev, int userID)
+        {
+            return _context.GetGuestByEvent(ev, userID);
+        }
+    }
+}
