f1e074f431a9442d5eb4c003fa45c73babee145f
Dino Spong <dino.spong@student.fontys.nl>
2015-11-05 11:03:07 +0100

    SMS minor fix

--- src/ICT4Events/Views/SocialSystem/Controls/MakePost.cs
+++ src/ICT4Events/Views/SocialSystem/Controls/MakePost.cs
@@ -59,9 +59,8 @@ namespace ICT4Events.Views.SocialSystem.Controls
         {
             var uploadFile = new OpenFileDialog
             {
-                Title = @"Media uploaden",
-                Filter =
-                    @"Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png | Audio files (*.wav, *.mp3) | *.wav; *.mp3 | Video files (*.mp4) | *.mp4 | All Files | *.* "
+                Title = "Media uploaden",
+                Filter = "Image files|*.jpg;*.jpeg;*.jpe;*.jfif;*.png|Audio files|*.mp3|Video files|*.mp4"
             };
             string[] imageEx = { ".jpg", ".jpeg", ".jpe", ".jfif", ".png" };
 
