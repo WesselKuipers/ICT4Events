193ac6ac0165db84e0ccc6a0780eca4c2b938e24
Dino Spong <dino.spong@student.fontys.nl>
2015-11-06 11:20:54 +0100

    MediaLogic added

--- src/ICT4Events/Views/SocialSystem/Controls/Catalogue.cs
+++ src/ICT4Events/Views/SocialSystem/Controls/Catalogue.cs
@@ -1,24 +1,28 @@
 ﻿using System;
 using System.Windows.Forms;
+using SharedModels.Enums;
 using SharedModels.Models;
 
 namespace ICT4Events.Views.SocialSystem.Controls
 {
     public partial class Catalogue : UserControl
     {
-        private readonly User _activeUser;
+        private readonly User _user;
         private readonly Event _event;
 
-        public Catalogue(Event ev,User activeUser)
+        public Catalogue(Event ev,User user)
         {
             InitializeComponent();
-            _activeUser = activeUser;
+            _user = user;
             _event = ev;
         }
 
         private void Catalogue_Load(object sender, EventArgs e)
         {
-
+            if (_user.Permission == PermissionType.User)
+            {
+                
+            }
         }
     }
 }
---
+++ src/SharedModels/Logic/MediaLogic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedModels.Data.ContextInterfaces;
+using SharedModels.Data.OracleContexts;
+using SharedModels.Models;
+
+namespace SharedModels.Logic
+{
+    public class MediaLogic
+    {
+        private IMediaContext _context;
+
+        public MediaLogic()
+        {
+            _context = new MediaOracleContext();
+        }
+
+        public MediaLogic(IMediaContext context)
+        {
+            _context = context;
+        }
+
+        public List<Media> GetAllByGuest(Guest guest)
+        {
+            return _context.GetAllByGuest(guest);
+        }
+    }
+}
