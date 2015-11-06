2bd24bde4a1fbd5f30487a55cfd91cb7a93c42a8
Dino Spong <dino.spong@student.fontys.nl>
2015-11-06 10:29:49 +0100

    Catalogue added

---
+++ src/ICT4Events/Views/SocialSystem/Controls/Catalogue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+using SharedModels.Models;
+
+namespace ICT4Events.Views.SocialSystem.Controls
+{
+    public partial class Catalogue : UserControl
+    {
+        private readonly User _activeUser;
+        private readonly Event _event;
+
+        public Catalogue(Event ev,User activeUser)
+        {
+            InitializeComponent();
+            _activeUser = activeUser;
+            _event = ev;
+        }
+
+        private void Catalogue_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
